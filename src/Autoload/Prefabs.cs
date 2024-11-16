7984c10c462d72549438b2b88fd326811c4b64b4
Tom Rijnbeek <tom@tomrijnbeek.me>
2024-11-16 23:38:56 +0100

    ⭕ Add globals

---
+++ src/Autoload/Prefabs.cs
@@ -0,0 +1,11 @@
+using Godot;
+
+namespace HalfNibbleGame.Autoload;
+
+public partial class Prefabs : Node
+{
+    public override void _Ready()
+    {
+        Global.Services.ProvidePersistent(this);
+    }
+}
